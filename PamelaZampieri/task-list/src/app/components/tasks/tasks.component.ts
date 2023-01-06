79facd7e1379fc20b1f6b65c2866503586d4050e
Pamela Zampieri <pame6_46@hotmail.com>
2023-01-06 19:57:03 -0300

    componente de lista de items

--- PamelaZampieri/task-list/src/app/components/tasks/tasks.component.ts
+++ PamelaZampieri/task-list/src/app/components/tasks/tasks.component.ts
@@ -2,17 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
 
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 
-export class TasksComponent implements OnInit{
+export class TasksComponent implements OnInit {
   tasks: Task[] = TASKS;
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void {
 
