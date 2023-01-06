30de846299ed0ea9fecc7306fe89caf8bb1d3d93
Pamela Zampieri <pame6_46@hotmail.com>
2023-01-06 19:10:18 -0300

    Lista de items

--- PamelaZampieri/task-list/src/app/Task.ts
+++ PamelaZampieri/task-list/src/app/Task.ts
@@ -1,5 +1,5 @@
 export interface Task {
-  id: number; //Cuando creamos una task viene con un id, que podría no venir
+  id: number; //Cuando creamos una task viene con un id, que podría no venir.
   text: string;
   day: string;
   reminder: boolean;
