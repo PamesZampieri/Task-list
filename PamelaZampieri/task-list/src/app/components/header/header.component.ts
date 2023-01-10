d60ff3029fa0582cfb159869fe71cd3f181ef8db
Pamela Zampieri <pame6_46@hotmail.com>
2023-01-10 15:08:18 -0300

    se crean servicio ui y componentes: add-task, about y footer

--- PamelaZampieri/task-list/src/app/components/header/header.component.ts
+++ PamelaZampieri/task-list/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,15 +11,26 @@ import { Component, OnInit } from '@angular/core';
 
 export class HeaderComponent implements OnInit {
 
-  title: string = 'Task List';
+  title: string = 'My Task List';
+  showAddTask: boolean = true;
+  suscription?: Subscription;
 
-  constructor() { }
+  constructor(
+    private uiService: UiService,
+    private router: Router
+  ) {
+    this.suscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
 
   }
 
   toggleAddTask() {
-    console.log("toggleAddTask!");
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
